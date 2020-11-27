b3cb8a13802840ce78859830281c59e617cae155
Rita-12397 <rhategekimana920@daviscollege.com>
2020-11-27 08:23:07 +0200

    added specific country

---
+++ lib/views/models/place.models.dart
@@ -0,0 +1,10 @@
+import 'package:visistafri/views/models/mostVisited.models.dart';
+
+class MostVisted {
+  String name;
+  List<Place> places;
+  MostVisted(
+    this.name,
+    this.places,
+  );
+}
